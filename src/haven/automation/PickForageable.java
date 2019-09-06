880b83f6416438e0fcc6e2f6c0d0a2653d95d28a
not_a_cat <fancy.zanta@gmail.com>
2019-09-07 02:58:13 +0300

    Use visitor gate with hotkey

--- src/haven/automation/PickForageable.java
+++ src/haven/automation/PickForageable.java
@@ -18,7 +18,7 @@ import java.util.Iterator;
 
 public class PickForageable implements Runnable {
     private GameUI gui;
-    public static final HashSet<String> gates = new HashSet(Arrays.asList("brickwallgate", "drystonewallgate", "drystonewallbiggate", "palisadegate", "palisadebiggate", "polegate", "polebiggate"));
+    public static final HashSet<String> gates = new HashSet(Arrays.asList("brickbiggate", "brickwallgate", "drystonewallgate", "drystonewallbiggate", "palisadegate", "palisadebiggate", "polegate", "polebiggate"));
     public PickForageable(GameUI gui) {
         this.gui = gui;
     }
@@ -30,8 +30,10 @@ public class PickForageable implements Runnable {
             if (gui.map.player() == null)
                 return;//player is null, possibly taking a road, don't bother trying to do all of the below.
             for (Gob gob : gui.map.glob.oc) {
+                /* unnecesarry check here?
                 if (gob.type == Type.TAMEDANIMAL)
                     continue; //don't evaluate tamed horses
+                    */
                 Resource res = null;
                 boolean gate = false;
                 boolean cart = false;
@@ -43,14 +45,15 @@ public class PickForageable implements Runnable {
                     CheckListboxItem itm = Config.icons.get(res.basename());
                     Boolean hidden = Boolean.FALSE;
                     if(!Config.disablegatekeybind)
-                    gate = gates.contains(res.basename());
+                        gate = gates.contains(res.basename());
                     if(!Config.disablecartkeybind)
-                    cart = res.basename().equals("cart");
+                        cart = res.basename().equals("cart");
                     if (itm == null)
                         hidden = null;
                     else if (itm.selected)
                         hidden = Boolean.TRUE;
 
+                    /*don't ignore open/close visitor gates
                     try {
                         if (gate) {
                             for(Gob.Overlay ol : gob.ols){
@@ -63,10 +66,11 @@ public class PickForageable implements Runnable {
                     } catch (Exception fucknulls) {
                         fucknulls.printStackTrace();
                     }
+                    */
 
                     if (hidden == null && res.name.startsWith("gfx/terobjs/herbs") || (hidden == Boolean.FALSE && !res.name.startsWith("gfx/terobjs/vehicle") && !cart) || gate || cart) {
                         double distFromPlayer = gob.rc.dist(gui.map.player().rc);
-                        if (distFromPlayer <= 20 * 11 && (herb == null || distFromPlayer < herb.rc.dist(gui.map.player().rc)))
+                        if (distFromPlayer <= 40 * 11 && (herb == null || distFromPlayer < herb.rc.dist(gui.map.player().rc)))
                             herb = gob;
                     }
                 }
@@ -88,7 +92,7 @@ public class PickForageable implements Runnable {
             if (herb.getres().basename().contains("goosebarnacle") && Config.autopickbarnacles)
                 gui.map.startMusselsPicker(herb);
             if (herb.getres().basename().contains("cattail") && Config.autopickcattails)
-                gui.map.startMusselsPicker(herb);*/
+            gui.map.startMusselsPicker(herb);*/
         }
     }
 }
